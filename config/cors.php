3b9ddb330d59a6cff3c8c5898d767abf0390876b
NONAME <System>
2026-06-05 09:31:42 +0700

    open cors

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter(array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', 'https://laporan-dpmptsp.web.app,https://laporan-dpmptsp.firebaseapp.com')))),
+    'allowed_origins' => array_filter(array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', '*')))),
 
     'allowed_origins_patterns' => [],
 
